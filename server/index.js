43d27380b1594297617c52f5ba93501cb6b4673f
Shubham Vishwakarma <skv6621@gmail.com>
2024-09-11 23:00:23 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const RegisterModel = require('./models/Register')
+
+const app = express()
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://skv66218828:skv66218828@cluster0.vhikf8f.mongodb.net/CRUD-APP?retryWrites=true&w=majority&appName=Cluster0');
+
+
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    RegisterModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            res.json("Already have an account")
+        } else {
+            RegisterModel.create({name: name, email: email, password: password})
+            .then(result => res.json(result))
+            .catch(err => res.json(err))
+        }
+    }).catch(err => res.json(err))
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is Running")
+})
