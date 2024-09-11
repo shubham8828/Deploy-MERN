b066524a1752e886cc15290f1484c827851faaa9
Shubham Vishwakarma <143716883+shubham8828@users.noreply.github.com>
2024-09-11 23:15:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require('./models/Register')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend-dun.vercel.app/"],
+        origin: ["https://deploy-mern-frontend-dun.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
